b54711ab42dac184aec6f50925a37c837292f116
MSmed <mattias.smedman@gmail.com>
2023-02-17 15:58:35 +0100

    feat: Add Interfaces for repositories

---
+++ Chinook/Chinook/Repositories/ICrudRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Repositories
+{
+    public interface ICrudRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+    }
+}
