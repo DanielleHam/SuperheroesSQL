c3db5998d6a6694f0807e8c23775f5e35a76a031
MSmed <mattias.smedman@gmail.com>
2023-02-17 15:59:15 +0100

    feat: Add Customer Repository
    Can get one by id or all users

---
+++ Chinook/Chinook/Concrete Repositories/CustomerRepositor.cs
@@ -0,0 +1,67 @@
+﻿using Chinook.Models;
+using Chinook.Repositories;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Concrete_Repositories
+{
+    public class CustomerRepositor : ICustomerRepository
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+
+        public Customer Get(int id)
+        {
+            using var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            var sql = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer WHERE CustomerId = @id";
+            
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            using SqlDataReader reader = command.ExecuteReader();
+
+            var result = new Customer();
+            while(reader.Read())
+            {
+                result = new Customer(
+                    reader.GetInt32(0),
+                    (!reader.IsDBNull(1) ? reader.GetString(1) : "No first name"),
+                    (!reader.IsDBNull(2) ? reader.GetString(2) : "No last name"),
+                    (!reader.IsDBNull(3) ? reader.GetString(3) : "No country"),
+                    (!reader.IsDBNull(4) ? reader.GetString(4) : "No postal code"),
+                    (!reader.IsDBNull(5) ? reader.GetString(5) : "No phone number"),
+                    (!reader.IsDBNull(6) ? reader.GetString(6) : "No email")
+                    );
+            }
+
+            return result;
+             
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            using var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            var sql = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer";
+            using var command = new SqlCommand(sql, connection);
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                yield return new Customer(
+                    reader.GetInt32(0),
+                    (!reader.IsDBNull(1) ? reader.GetString(1) : "No first name"),
+                    (!reader.IsDBNull(2) ? reader.GetString(2) : "No last name"),
+                    (!reader.IsDBNull(3) ? reader.GetString(3) : "No country"),
+                    (!reader.IsDBNull(4) ? reader.GetString(4) : "No postal code"),
+                    (!reader.IsDBNull(5) ? reader.GetString(5) : "No phone number"),
+                    (!reader.IsDBNull(6) ? reader.GetString(6) : "No email")
+                    );
+            }
+
+        }
+    }
+}
