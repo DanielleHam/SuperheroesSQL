b87f26c165da8bcd23c6bc3b9903b02910ce2ea3
MSmed <mattias.smedman@gmail.com>
2023-02-17 16:02:00 +0100

    feat: Testing code

--- Chinook/Chinook/Program.cs
+++ Chinook/Chinook/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Chinook.Concrete_Repositories;
+using Chinook.Models;
+using Microsoft.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+var customerRepositories = new CustomerRepositor { ConnectionString = GetConnectionString() };
+
+var allCustomers = customerRepositories.GetAll();
+var oneCustomer = customerRepositories.GetById(1);
+foreach(var customer in allCustomers)
+{
+    Console.WriteLine($"{customer.customerId} : {customer.firstName} : {customer.lastName} : {customer.country} : {customer.postalCode} : {customer.phoneNumber} : {customer.email}");
+}
+
+Console.WriteLine(oneCustomer.firstName);
+
+static string GetConnectionString()
+{
+    var builder = new SqlConnectionStringBuilder
+    {
+        DataSource = "localhost\\SQLEXPRESS",
+        InitialCatalog = "Chinook",
+        IntegratedSecurity = true,
+        TrustServerCertificate = true
+    };
+
+    return builder.ConnectionString;
+}
