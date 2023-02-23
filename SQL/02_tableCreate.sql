c868479433307aba5cc5ba31b680ccdf18673d40
Mattias Smedman <mattias.smedman@gmail.com>
2023-02-23 13:18:42 +0100

    chore: Change SQL to use nvarchar

--- SQL/02_tableCreate.sql
+++ SQL/02_tableCreate.sql
@@ -1,18 +1,18 @@
 USE SuperherosDB
 CREATE TABLE Superhero(
 	SuperheroId int IDENTITY(1, 1) PRIMARY KEY,
- 	SuperheroName varchar(30) NOT NULL, 
-	SuperheroAlias varchar(15) NOT NULL,
-	SuperheroOrigin varchar(30) NOT NULL
+ 	SuperheroName nvarchar(30) NOT NULL, 
+	SuperheroAlias nvarchar(15) NOT NULL,
+	SuperheroOrigin nvarchar(30) NOT NULL
 );
 
 CREATE TABLE Assistant(
 	AssistantId int IDENTITY(1, 1) PRIMARY KEY,
- 	AssistantName varchar(30) NOT NULL, 
+ 	AssistantName nvarchar(30) NOT NULL, 
 );
 
 CREATE TABLE Power(
 	PowerId int IDENTITY(1, 1) PRIMARY KEY,
- 	PowerName varchar(30) NOT NULL, 
-	PowerDescription varchar(80) NOT NULL
+ 	PowerName nvarchar(30) NOT NULL, 
+	PowerDescription nvarchar(80) NOT NULL
 );
